727442c85eeef3af92fe1f51b81244e7200b7e8e
aMetallurgist <joshua.d1434@gmail.com>
2022-08-21 13:06:34 -0700

    Add optional main function

--- my_project/hello_test.cc
+++ my_project/hello_test.cc
@@ -7,3 +7,20 @@ TEST(HelloTest, BasicAssertions) {
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
+
+/*Optional main function. 
+CMakeLists.txt already linked gtest_main which implements
+the main function for us. If you need custom code within main
+then you are responsible for calling InitGoogleTest and
+RUN_ALL_TESTS(). 
+
+Replace gtest_main with gtest within CMakeList.txt's
+target_link_libraries declaration if using custom main.
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+    // Your custom code
+	return RUN_ALL_TESTS();
+}
+*/
